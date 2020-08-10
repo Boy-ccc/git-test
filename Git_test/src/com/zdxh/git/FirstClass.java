3e735ee4c5017fa6999202c9e9fea323d166eadb
Boy <1232331222@qq.com>
2020-08-10 16:37:20 +0800

    第一个idea工程

---
+++ Git_test/src/com/zdxh/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.zdxh.git;
+
+/**
+ * @author boy
+ * @create 2020-08-10 16:29
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("I want to");
+        System.out.println("I want to");
+        System.out.println("I want to");
+        System.out.println("I want to");
+        System.out.println("I want to");
+        System.out.println("I want to");
+    }
+
+}
