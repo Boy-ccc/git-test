0651987fe3b67dfd10611e84513c88b5a7df0e9f
Boy <1232331222@qq.com>
2020-08-10 16:58:36 +0800

    添加了Second类

---
+++ Git_test/src/com/zdxh/git/Second.java
@@ -0,0 +1,13 @@
+package com.zdxh.git;
+
+/**
+ * @author boy
+ * @create 2020-08-10 16:57
+ */
+public class Second {
+
+    public void test() {
+        System.out.println("hello second");
+    }
+
+}
